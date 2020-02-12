a2625c7c457ce5b91b78a2d32d10c403bc4edbe3
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-11 21:08:45 -0600

    [ADD] concerns to the response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  private 
+
+  def authenticate_request
+    @current_user = AuthorizeRequest.call(request.headers).result
+    json_response({ error: 'Not Authorized' }, 401) unless @current_user
+  end
 end
