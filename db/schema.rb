8460338760d8a87b9e9241baf88cfee503efb90f
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-11 21:03:46 -0600

    [ADD] weight, height, bmi and new password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_211554) do
+ActiveRecord::Schema.define(version: 2020_02_11_224616) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "weight"
+    t.float "height"
+    t.float "bmi"
   end
 
 end
