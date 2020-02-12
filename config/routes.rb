af7fa2664d626209fe407dbe617b2ac15a858ee3
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-12 13:17:16 -0600

    [ADD] routes for category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
   resources :users
+  get 'users/:id/category', to: 'users#category'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed max seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+puts 'Creating Categories'
+
+created = Category.create!([
+  { name: "Very severely underweight",             min: 0.00, max: 15.0 },
+  { name: "Severely underweight",                  min: 15.0, max: 16.0 },
+  { name: "Underweight",                           min: 16.0, max: 18.5 },
+  { name: "Normal (healthy weight)",               min: 18.5, max: 25.0 },
+  { name: "Overweight" ,                           min: 25.0, max: 30.0 },
+  { name: "Obese Class I (Moderately obese)",      min: 30.0, max: 35.0 },
+  { name: "Obese Class II (Severely obese)",       min: 35.0, max: 40.0 },
+  { name: "Obese Class III (Very severely obese)", min: 40.0, max: 45.0 },
+  { name: "Obese Class IV (Morbidly obese)",       min: 45.0, max: 50.0 },
+  { name: "Obese Class V (Super obese)",           min: 50.0, max: 60.0 },
+  { name: "Obese Class VI (Hyper obese)",          min: 60.0, max: 1000 },
+])
+
